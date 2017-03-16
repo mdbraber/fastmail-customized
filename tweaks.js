f8a667d9ef86a0ed13aeafd8a61945858cc1afd0
Maarten den Braber <m@mdbraber.com>
2017-03-16 11:45:26 +0100

    Found a better way + added comments

--- tweaks.js
+++ tweaks.js
@@ -1,31 +1,49 @@
-// It's hacky-to-the-max but works in Fluid!
-console.log("Running Fastmail Tweaks");
 var ready = false;
 
 function init() {
 
-	// quit if this function has already been called
-	if (!document.getElementById("mail") || ready) return;
+    // quit if this function has already been called
+    if (!document.getElementById("mail") || ready) { return; }
 
-	// flag this function so we don't do the same thing twice
-	ready = true;
+    // flag this function so we don't do the same thing twice
+    ready = true;
 
-	// kill the timer
-	if (_timer) clearInterval(_timer);
+    // kill the timer
+    if (_timer) { clearInterval(_timer); }
 
-	// Moved Saved searches right below the Inbox
-	O.require( 'base', function () {
-		FastMail.SavedSearch.implement({ precedence: 1 }, true );
-	});
+    O.require( 'base', function () {
+        // order SavedSearch on top (directly below Inbox)
+        FastMail.SavedSearch.implement({ precedence: 1 }, true );
+        // remap the 'e' key in threadView to 'y'
+        FastMail.mail.threadViewController.keys.y = FastMail.mail.threadViewController.keys.e;
+        // remove the 'e' shortcut in threadView (we need that to attach the archive shortcut,
+        // otherwise it will attach it again when switching views
+        // this way it seems also 'e' gets remapped to 'y' in thread view
+        delete FastMail.mail.threadViewController.keys.e;
+    });
 
-	// For some reason we have to wait otherwise the array gets duplicated (probably by Fastmail still initializing)
-	setTimeout(function(){ FastMail.keyboardShortcuts._shortcuts.e=FastMail.keyboardShortcuts._shortcuts.y }, 2000);
-	document.getElementById("v12").accessKey = "e";
+    if (FastMail.keyboardShortcuts._shortcuts.y != undefined) {
+         
+        // save the object of 'y'-shortcut as the archiveKey
+        var archiveKey = FastMail.keyboardShortcuts._shortcuts.y;
+        // adjust the shortcut key for archive
+        archiveKey[0][0].shortcut = "e";
+        
+        // deregister the 'y'=shortcut
+        FastMail.keyboardShortcuts.deregister("y",FastMail.keyboardShortcuts._shortcuts.y[0][0],FastMail.keyboardShortcuts._shortcuts.y[0][1]);
+        // if 'e'-shortcut is already defined, remove it
+        if(FastMail.keyboardShortcuts._shortcuts.e != undefined) {
+            FastMail.keyboardShortcuts.deregister("e",FastMail.keyboardShortcuts._shortcuts.e[0][0],FastMail.keyboardShortcuts._shortcuts.e[0][1]);
+        }
+        // register the new archive shortcut
+        FastMail.keyboardShortcuts.register("e",archiveKey[0][0],archiveKey[0][1]);
+    }
 
 };
 
+// there is no good domloaded support in Fluid so we need a timer until ready
 var _timer = setInterval(function() {
-	if (/loaded|complete/.test(document.readyState)) {
-		init(); // call the onload handler
-	}
+  if (/loaded|complete/.test(document.readyState)) {
+    init(); // call the onload handler
+  }
 }, 500);
