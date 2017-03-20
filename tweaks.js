e1b09542023525f42c9f3f89c1f6ff3a8383e8bf
Maarten den Braber <m@mdbraber.com>
2017-03-20 14:50:47 +0100

    Update mailbox name if there is unread mail in other folders

--- tweaks.js
+++ tweaks.js
@@ -1,3 +1,4 @@
+//use strict;
 var ready = false;
 
 function init() {
@@ -11,7 +12,7 @@ function init() {
     // kill the timer
     if (_timer) { clearInterval(_timer); }
 
-    O.require( 'base', function () {
+    O.require( 'base', function () {                            
         // order SavedSearch on top (directly below Inbox)
         FastMail.SavedSearch.implement({ precedence: 1 }, true );
 
@@ -27,18 +28,16 @@ function init() {
         FastMail.views.mailbox.childViews[0].registerConfig("short",{left: ["selectAll","filter","pin"]});
        
         // register observer to update the mailboxName
-        FastMail.mail.screenController.addObserverForKey("mailboxTitle",window,"switchMailbox");
+        FastMail.mail.screenController.addObserverForKey("mailboxTitle",window,"updateMailbox");
 
-        // Save the 'e' shortcut object 
-        window._e = FastMail.keyboardShortcuts._shortcuts.e;
     });
 
-    if (FastMail.keyboardShortcuts._shortcuts.y != undefined) {
+    if (FastMail.keyboardShortcuts._shortcuts.y !== undefined) {
          
         // save the object of 'y'-shortcut as the archiveKey
-        var archiveKey = FastMail.keyboardShortcuts._shortcuts.y;
+        window._archiveKey = FastMail.keyboardShortcuts._shortcuts.y;
         // adjust the shortcut key for archive
-        archiveKey[0][0].shortcut = "e";
+        window._archiveKey[0][0].shortcut = "e";
         // save the object of 'e'-shortcut as the expandKey
         //var expandKey = FastMail.keyboardShortcuts._shortcuts.e;
         // adjust the shortcut key for expand
@@ -47,11 +46,11 @@ function init() {
         // deregister the 'y'=shortcut
         FastMail.keyboardShortcuts.deregister("y",FastMail.keyboardShortcuts._shortcuts.y[0][0],FastMail.keyboardShortcuts._shortcuts.y[0][1]);
         // if 'e'-shortcut is already defined, remove it
-        if(FastMail.keyboardShortcuts._shortcuts.e != undefined) {
+        if(FastMail.keyboardShortcuts._shortcuts.e !== undefined) {
             FastMail.keyboardShortcuts.deregister("e",FastMail.keyboardShortcuts._shortcuts.e[0][0],FastMail.keyboardShortcuts._shortcuts.e[0][1]);
         }
         // register the new archive shortcut
-        FastMail.keyboardShortcuts.register("e",archiveKey[0][0],archiveKey[0][1]);
+        FastMail.keyboardShortcuts.register("e",window._archiveKey[0][0],window._archiveKey[0][1]);
         // register the new expand shortcut
         //FastMail.keyboardShortcuts.register("y",expandKey[0][0],expandKey[0][1]);
         
@@ -60,57 +59,95 @@ function init() {
         
     }
 
-    // only display "All mail" and "Unread" in the filter selector
-    // (I'm using different folders for Mailinglists and Notifications)
-    FastMail.views.mailbox.childViews[0].redraw();
-
     // Insert the mailbox name after the "selectAll" button (because we're not showing the sidebar)
     document.getElementById("selectAll").insertAdjacentHTML('afterend','<span id="mailboxName">'+document.querySelector('.app-source.app-source--depth0.is-selected span').innerText+'</span>');
 
     // Remove the placeholder in search (it overlaps w/ saved searches when we quick-switch)
     document.querySelector("#v6 input").placeholder = ""
-    
-    // skip - we do this in CSS
-    //if (document.getElementById("FilterSelector") != undefined) {
-    //    var filterElements = document.getElementById("FilterSelector").getElementsByClassName("v-PushSelect-option");
-    //    for (i = filterElements.length - 1 ; i > 1; i--) {
-    //        console.log(i + " " + filterElements[i]);
-    //        elem = filterElements[i];
-    //        elem.remove();
-    //    }
-    //}
 
+    // register observer for badge count changes
+    // FIXME: this can probably also been done more nice on the Fastmail object?
+    // folder: Inbox
+    window.createObserver(document.querySelector(".v-FolderSource--inbox > .v-FolderSource-badge"));
+    // folder: +Later
+    window.createObserver(document.querySelector("#v104 span.v-FolderSource-badge"));
+    // folder: +News
+    window.createObserver(document.querySelector("#v105 span.v-FolderSource-badge"));
+        
+    // only display "All mail" and "Unread" in the filter selector
+    // (I'm using different folders for Mailinglists and Notifications)
+    FastMail.views.mailbox.childViews[0].redraw();
+
+    // Update the mailbox title
+    window.updateMailbox(null,null,FastMail.mail.screenController.mailboxTitle,FastMail.mail.screenController.mailboxTitle);
+
+}
+
+window.updateMailbox = function updateMailbox(obj,key,oldValue,newValue) {
+
+    var totalUnread = 0;
+    var totalUnreadString = "";
+    // Get count for Inbox
+    var inboxUnread = parseInt(document.querySelector('.v-FolderSource--inbox span.v-FolderSource-badge').textContent);
+    //console.log("inboxUnread: "+inboxUnread);
+    // Get count for others (+Later and +News)
+    var otherUnread = parseInt(document.querySelector('#v104 span.v-FolderSource-badge').textContent) + parseInt(document.querySelector('#v105 span.v-FolderSource-badge').textContent);
+    //console.log("otherUnread: "+otherUnread);
+    // Use an asterisk when there are unread mails in other folders
+    if(inboxUnread || otherUnread > 0) {
+        totalUnread = inboxUnread + otherUnread;
+        totalUnreadString = "*";
+    }
+
+    // Show the mailbox name first and then number of unread messages - like "Inbox (1)"
+    var regex = /\((\d*)\)\s(.*)/;
+    // Write the mailbox name and counts
+    document.getElementById("mailboxName").innerHTML = newValue.replace(regex, "$2 ($1)");
+    // Append totalUnreadString
+    var thisUnread = parseInt(newValue.replace(regex, "$1")) || 0;
+    //console.log("newValue: "+newValue+" / totalUnread: "+totalUnread+" / thisUnread:"+thisUnread);
+    if(totalUnread > thisUnread) {
+        document.getElementById("mailboxName").innerHTML += totalUnreadString;
+    }
+
+/*
+    // Save the Archive-shortcut for reference when registering/dergistering
+    if(window._e == undefined && FastMail.keyboardShortcuts._shortcuts.e !== undefined) {
+        window._e = FastMail.keyboardShortcuts._shortcuts.e;
+    } else {
+        console.log("ERROR: lost the Archive-shortcut object");
+        console.log("window._e: "+window._e.toString());
+        console.log("FastMail.keyboardShortcuts._shortcuts.e: "+FastMail.keyboardShortcuts._shortcuts.e);
+    }
+*/
+
+    if(newValue.contains("Inbox")) {
+        // Show the Archive-button
+        document.querySelector("#v12").style.display="";
+        document.querySelector("#v12 + span").style.display="";
+        // Register the Archive-shortcut
+        FastMail.keyboardShortcuts.register("e",window._archiveKey[0][0],window._archiveKey[0][1]);
+    } else {
+        // Hide the Archive-button
+        document.querySelector("#v12").style.display="none";
+        document.querySelector("#v12 + span").style.display="none";
+        // Deregister the Archive-shortcut
+        FastMail.keyboardShortcuts.deregister("e",window._archiveKey[0][0],window._archiveKey[0][1]);
+    }
 
 };
 
-window.switchMailbox = function updateMailboxName(obj,key,oldValue,newValue) {
+// mutation observer
+window.createObserver = function createObserver(t) {
+    var observer = new MutationObserver(function(mutations) {
+        //mutations.forEach(function(mutation) { 
+            //console.log("observed / "+t.toString());
+            window.updateMailbox(null,null,FastMail.mail.screenController.mailboxTitle,FastMail.mail.screenController.mailboxTitle);
+        //});
+    });    
+    observer.observe(t, {attributes: false, childList: true, characterData: true});
+};
 
-    console.log(oldValue + "/" + newValue);
-
-    regex = /\((\d*)\) (.*)/;
-    document.getElementById("mailboxName").innerHTML = newValue.replace(regex, "$2 ($1)");
-
-    if(!window._e) {
-        window._e = FastMail.keyboardShortcuts._shortcuts.e;
-    }
-
-    if(newValue.contains("Inbox")) {
-        document.querySelector("#v12").style.display="";
-        document.querySelector("#v12 + span").style.display="";
-        //if(FastMail.keyboardShortcuts._shortcuts.e != undefined) {
-            console.log("register e");
-            FastMail.keyboardShortcuts.register("e",window._e[0][0],window._e[0][1]);
-        //}
-    } else {
-        document.querySelector("#v12").style.display="none";
-        document.querySelector("#v12 + span").style.display="none";
-        //if(FastMail.keyboardShortcuts._shortcuts.e == undefined) {
-            console.log("deregister e");
-            FastMail.keyboardShortcuts.deregister("e",window._e[0][0],window._e[0][1]);
-        //}
-    }
-
-}
 
 // there is no good domloaded support in Fluid so we need a timer until ready
 var _timer = setInterval(function() {
